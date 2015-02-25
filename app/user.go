4e60844e6c5e39aff2f4b5935f43e40b52cfbe29
Austin Dizzy <dizzy@wow.com>
2015-02-25 23:55:19 +0000

    Added all campaign staff emails.

--- app/user.go
+++ app/user.go
@@ -9,7 +9,14 @@ func IsWVUStudent(email string) bool {
 }
 
 func IsCampaignStaff(email string) bool {
-    staffEmails := []string{"absiford@mix.wvu.edu", "whardy1@mix.wvu.edu"}
+    staffEmails := []string{"absiford@mix.wvu.edu", "whardy1@mix.wvu.edu",
+    "adsutherland@mix.wvu.edu", "kalaska@mix.wvu.edu", "ilkovtoniuk@mix.wvu.edu",
+    "jtheenan@mix.wvu.edu", "reelkins@mix.wvu.edu", "krash@mix.wvu.edu",
+    "cmorlock@mix.wvu.edu", "sricha12@mix.wvu.edu", "tcmccloud@mix.wvu.edu",
+    "aionderik@mix.wvu.edu", "jleach6@mix.wvu.edu", "jamcguire@mix.wvu.edu",
+    "mwrogers@mix.wvu.edu", "sjcrandall@mix.wvu.edu", "aherric1@mix.wvu.edu",
+    "jlriseberg@mix.wvu.edu", "nrmcdill@mix.wvu.edu", "clammer1@mix.wvu.edu",
+    "rlburky@mix.wvu.edu", "aleccneu@gmail.com", "scottbraxton2015@gmail.com"}
     for _, v := range staffEmails {
         if email == v {
             return true

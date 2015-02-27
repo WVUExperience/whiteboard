55b887213f258506c6a98cebaaab4afd4fc25180
Austin Dizzy <dizzy@wow.com>
2015-02-27 06:47:31 +0000

    Fixed all upvote arrows being colored on vote.
    Previously, because of a terrible selector choice,
    all upvote arrows on the page were colored when
    upvoting any post at all. This fixes that by using
    the unique slug selector to color the arrow.

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,7 @@ $(function(){
             if(result.success) {
                 var count = $('[data-slug="'+slug+'"]').next();
                 $(count).text(Number.parseInt($(count).text())+1);
-                $('.votes > i').css({"color":"orangered"});
+                $('[data-slug="'+slug+'"]').css({"color":"orangered"});
             } else if(result.error == NotLoggedIn) {
                 alert("Please log in to vote. You will now be redirected to login.\nPlease log in using your student MIX account.");
                 document.location = "/login?returnUrl=" + encodeURIComponent(document.URL)
